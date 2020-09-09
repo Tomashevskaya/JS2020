9f2b23390ea62fdc9baf62832717557b9a258a8e
Tomashevskaya <Dasha.tomashewsckaya@yandex.ru>
2020-09-09 20:31:59 +0300

    Add js

---
+++ js/index.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	console.log(1);
+});
+
+
+// var condition0 = true;
+// var condition1 = condition0 && false;
+
+// var condition2 = condition0 || condition1;
+
+// var condition3 = condition2 || (condition1 && condition0);
+
+// var condition3 = !(condition1 && condition0);
+
+
+// var num1 = 2;
+// var num2 = 15;
+
+// //7
+// var answer = sum(num1, num2);
+
+// //75
+// var answer2 = sum(60, num2);
+
+// var answer3 = sum(50, 100);
+
+// //167 - 1050
+// var answer4 = sum(answer, answer3) - sum(5, 7);
+
+// var answer4 = sum(sum(60, 80), sum(1, 1)) - sum(5, 7);
+
+// function sum(anyNumber1, anyNumber2){
+	// return anyNumber1 + anyNumber2;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
